8600bf29612376ae70f11c90a302acfce24b82c2
leekh <leekh4232@gmail.com>
2020-08-24 08:47:09 +0900

    log4j버전변경,명세서를 하나의 시트에 통합 출력하도록 수정

--- README.md
+++ README.md
@@ -5,11 +5,9 @@ MySQL의 테이블 구조를 조회하여 엑셀로 명세서를 저장해주는
 - 저작도구: Eclipse
 - 사용언어: JAVA
 
-사용시에는 config.xml 파일의 데이터베이스 접속 정보를 수정하여 사용하시기 바랍니다. 자세한 사용법은 다음의 URL을 참고하시면 됩니다.
+사용시에는 config.xml 파일의 데이터베이스 접속 정보를 수정하여 사용하시기 바랍니다.
 
-[itpaper_link]: http://www.itpaper.co.kr/mysql-테이블-명세서-생성-프로그램
-
-![Alt text](http://www.itpaper.co.kr/wp-content/uploads/2016/06/2b1561e56127e458e66568948d7ff24b.png)
+![Alt text](img.png)
 
 ## 사용된 라이브러리
 - https://github.com/virtuald/curvesapi
--- src/Main.java
+++ src/Main.java
@@ -28,260 +28,276 @@ import kr.co.itpaper.mysqldocument.service.TableService;
 import kr.co.itpaper.mysqldocument.service.impl.TableServiceImpl;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		// parent component of the dialog
-		JFileChooser fileChooser = new JFileChooser();
-		fileChooser.setDialogTitle("파일이 저장될 경로를 지정하세요.");   
-		 
-		int userSelection = fileChooser.showSaveDialog(null);
-		String saveFileName = null;
-		
-		if (userSelection == JFileChooser.APPROVE_OPTION) {
-		    File fileToSave = fileChooser.getSelectedFile();
-		    saveFileName = fileToSave.getAbsolutePath();
-		} else {
-			return;
-		}
-		
-		if (saveFileName.lastIndexOf(".xlsx") == -1) {
-			saveFileName += ".xlsx";
-		}
+    @SuppressWarnings("deprecation")
+    public static void main(String[] args) {
 
-		SqlSession sqlSession = MyBatisConnectionFactory.getSqlSession();
-		Logger logger = LoggerFactory.getLogger(Main.class.getName());
+        // parent component of the dialog
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("파일이 저장될 경로를 지정하세요.");
 
-		TableService tableService = new TableServiceImpl(sqlSession, logger);
+        int userSelection = fileChooser.showSaveDialog(null);
+        String saveFileName = null;
 
-		List<TableName> table = null; // 테이블 목록
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            File fileToSave = fileChooser.getSelectedFile();
+            saveFileName = fileToSave.getAbsolutePath();
+        } else {
+            return;
+        }
 
-		try {
-			table = tableService.selectTableList();
+        if (saveFileName.lastIndexOf(".xlsx") == -1) {
+            saveFileName += ".xlsx";
+        }
 
-			for (int i = 0; i < table.size(); i++) {
-				TableName tableItem = table.get(i);
-				// 테이블 목록 수 만큼 구조 조회 SQL 호출
-				tableItem.setStruct(tableService.selectTableStruct(tableItem.getTableName()));
-			}
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, e.getLocalizedMessage(), "확인", JOptionPane.ERROR_MESSAGE);
-			e.printStackTrace();
-			sqlSession.close();
-			return;
-		} finally {
-			sqlSession.close();
-		}
+        SqlSession sqlSession = MyBatisConnectionFactory.getSqlSession();
+        Logger logger = LoggerFactory.getLogger(Main.class.getName());
 
-		// -----------
+        TableService tableService = new TableServiceImpl(sqlSession, logger);
 
-		// 1차로 workbook을 생성
-		XSSFWorkbook workbook = new XSSFWorkbook();
+        List<TableName> table = null; // 테이블 목록
 
-		// 해더부분셀에 스타일을 주기위한 인스턴스 생성
-		XSSFCellStyle headerStyle = workbook.createCellStyle();
-		headerStyle.setAlignment(HorizontalAlignment.CENTER); // 스타일인스턴스의 속성
-		headerStyle.setFillForegroundColor(new XSSFColor(new java.awt.Color(255,255,0)));
-		headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-		headerStyle.setBorderRight(BorderStyle.THIN); // 테두리 설정
-		headerStyle.setBorderLeft(BorderStyle.THIN);
-		headerStyle.setBorderTop(BorderStyle.THIN);
-		headerStyle.setBorderBottom(BorderStyle.THIN);
-		XSSFFont font = workbook.createFont(); // 폰트 조정 인스턴스 생성
-		font.setBold(true);
-		font.setFontHeightInPoints((short) 16);
-		headerStyle.setFont(font);
+        try {
+            table = tableService.selectTableList();
 
-		// 가운데 정렬과 얇은 테두리를 위한 스타일 인스턴스 생성
-		XSSFCellStyle cellStyle0 = workbook.createCellStyle();
-		cellStyle0.setAlignment(HorizontalAlignment.CENTER);
-		cellStyle0.setBorderRight(BorderStyle.THIN);
-		cellStyle0.setBorderLeft(BorderStyle.THIN);
-		cellStyle0.setBorderTop(BorderStyle.THIN);
-		cellStyle0.setBorderBottom(BorderStyle.THIN);
-		XSSFFont font0 = workbook.createFont(); // 폰트 조정 인스턴스 생성
-		font0.setFontHeightInPoints((short) 14);
-		cellStyle0.setFont(font0);
+            for (int i = 0; i < table.size(); i++) {
+                TableName tableItem = table.get(i);
+                // 테이블 목록 수 만큼 구조 조회 SQL 호출
+                tableItem.setStruct(tableService.selectTableStruct(tableItem.getTableName()));
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getLocalizedMessage(), "확인", JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+            sqlSession.close();
+            return;
+        } finally {
+            sqlSession.close();
+        }
 
-		// 얇은 테두리를 위한 스타일 인스턴스 생성
-		XSSFCellStyle cellStyle1 = workbook.createCellStyle();
-		cellStyle1.setAlignment(HorizontalAlignment.LEFT);
+        // -----------
+
+        // 1차로 workbook을 생성
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        // 해더부분셀에 스타일을 주기위한 인스턴스 생성
+        XSSFCellStyle headerStyle = workbook.createCellStyle();
+        headerStyle.setAlignment(HorizontalAlignment.CENTER); // 스타일인스턴스의 속성
+        headerStyle.setFillForegroundColor(new XSSFColor(new java.awt.Color(255, 255, 0)));
+        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        headerStyle.setBorderRight(BorderStyle.THIN); // 테두리 설정
+        headerStyle.setBorderLeft(BorderStyle.THIN);
+        headerStyle.setBorderTop(BorderStyle.THIN);
+        headerStyle.setBorderBottom(BorderStyle.THIN);
+        XSSFFont font = workbook.createFont(); // 폰트 조정 인스턴스 생성
+        font.setBold(true);
+        font.setFontHeightInPoints((short) 16);
+        headerStyle.setFont(font);
+
+        // 가운데 정렬과 얇은 테두리를 위한 스타일 인스턴스 생성
+        XSSFCellStyle cellStyle0 = workbook.createCellStyle();
+        cellStyle0.setAlignment(HorizontalAlignment.CENTER);
+        cellStyle0.setBorderRight(BorderStyle.THIN);
+        cellStyle0.setBorderLeft(BorderStyle.THIN);
+        cellStyle0.setBorderTop(BorderStyle.THIN);
+        cellStyle0.setBorderBottom(BorderStyle.THIN);
+        XSSFFont font0 = workbook.createFont(); // 폰트 조정 인스턴스 생성
+        font0.setFontHeightInPoints((short) 14);
+        cellStyle0.setFont(font0);
+
+        // 얇은 테두리를 위한 스타일 인스턴스 생성
+        XSSFCellStyle cellStyle1 = workbook.createCellStyle();
+        cellStyle1.setAlignment(HorizontalAlignment.LEFT);
         cellStyle1.setBorderRight(BorderStyle.THIN);
         cellStyle1.setBorderLeft(BorderStyle.THIN);
         cellStyle1.setBorderTop(BorderStyle.THIN);
         cellStyle1.setBorderBottom(BorderStyle.THIN);
-		XSSFFont font1 = workbook.createFont(); // 폰트 조정 인스턴스 생성
-		font1.setFontHeightInPoints((short) 14);
-		cellStyle1.setFont(font1);
-		
-		XSSFRow row = null;
-		XSSFCell cell = null;
+        XSSFFont font1 = workbook.createFont(); // 폰트 조정 인스턴스 생성
+        font1.setFontHeightInPoints((short) 14);
+        cellStyle1.setFont(font1);
 
-		for (int i = 0; i < table.size(); i++) {
-			TableName name = table.get(i);
+        XSSFRow row = null;
+        XSSFCell cell = null;
+        
+        // 2차는 sheet생성
+        XSSFSheet sheet = workbook.createSheet("테이블명세서");
 
-			// 2차는 sheet생성
-			XSSFSheet sheet = workbook.createSheet(name.getTableName());
+        // 셀별 널비 정하기 헤더 그리기
+        sheet.setColumnWidth((short) 0, (short) 2000);
+        sheet.setColumnWidth((short) 1, (short) 5000);
+        sheet.setColumnWidth((short) 2, (short) 4000);
+        sheet.setColumnWidth((short) 3, (short) 3000);
+        sheet.setColumnWidth((short) 4, (short) 3000);
+        sheet.setColumnWidth((short) 5, (short) 5000);
+        sheet.setColumnWidth((short) 6, (short) 10000);
 
-			// 셀별 널비 정하기 헤더 그리기
-			sheet.setColumnWidth((short) 0, (short) 2000);
-			sheet.setColumnWidth((short) 1, (short) 5000);
-			sheet.setColumnWidth((short) 2, (short) 4000);
-			sheet.setColumnWidth((short) 3, (short) 3000);
-			sheet.setColumnWidth((short) 4, (short) 3000);
-			sheet.setColumnWidth((short) 5, (short) 5000);
-			sheet.setColumnWidth((short) 6, (short) 10000);
-			
-			sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 1));
-			sheet.addMergedRegion(new CellRangeAddress(0, 0, 2, 6));
-			sheet.addMergedRegion(new CellRangeAddress(1, 1, 0, 1));
-			sheet.addMergedRegion(new CellRangeAddress(1, 1, 2, 6));
-			
-			// 시트에 타이틀 행을 하나 생성한다.(i 값이 0이면 첫번째 줄에 해당)
-			int k = 0;
-			row = sheet.createRow((short) k++);
-			cell = row.createCell(0);
-			cell.setCellValue("TableName");
-			cell.setCellStyle(headerStyle);
-			
-			cell = row.createCell(1);
-			cell.setCellStyle(headerStyle);
-			
-			cell = row.createCell(2);
-			cell.setCellValue(name.getTableName());
-			cell.setCellStyle(cellStyle1);
-			
-			cell = row.createCell(3);
-			cell.setCellStyle(cellStyle1);
-			cell = row.createCell(4);
-			cell.setCellStyle(cellStyle1);
-			cell = row.createCell(5);
-			cell.setCellStyle(cellStyle1);
-			cell = row.createCell(6);
-			cell.setCellStyle(cellStyle1);
-			
-			row = sheet.createRow((short) k++);
-			cell = row.createCell(0);
-			cell.setCellValue("Description");
-			cell.setCellStyle(headerStyle);
-			
-			cell = row.createCell(1);
-			cell.setCellStyle(headerStyle);
-			
-			cell = row.createCell(2);
-			cell.setCellValue(name.getComment());
-			cell.setCellStyle(cellStyle1);
-			
-			cell = row.createCell(3);
-			cell.setCellStyle(cellStyle1);
-			cell = row.createCell(4);
-			cell.setCellStyle(cellStyle1);
-			cell = row.createCell(5);
-			cell.setCellStyle(cellStyle1);
-			cell = row.createCell(6);
-			cell.setCellStyle(cellStyle1);
-			
-			
-			row = sheet.createRow((short) k++);
+        sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 1));
+        sheet.addMergedRegion(new CellRangeAddress(0, 0, 2, 6));
+        sheet.addMergedRegion(new CellRangeAddress(1, 1, 0, 1));
+        sheet.addMergedRegion(new CellRangeAddress(1, 1, 2, 6));
 
-			cell = row.createCell(0);
-			cell.setCellValue("No");
-			cell.setCellStyle(headerStyle);
+        // 시트에 타이틀 행을 하나 생성한다.(i 값이 0이면 첫번째 줄에 해당)
+        int k = 0;
 
-			cell = row.createCell(1);
-			cell.setCellValue("FieldName");
-			cell.setCellStyle(headerStyle);
-			
-			cell = row.createCell(2);
-			cell.setCellValue("DataType");
-			cell.setCellStyle(headerStyle);
-			
-			cell = row.createCell(3);
-			cell.setCellValue("Null");
-			cell.setCellStyle(headerStyle);
-			
-			cell = row.createCell(4);
-			cell.setCellValue("Key");
-			cell.setCellStyle(headerStyle);
-			
-			cell = row.createCell(5);
-			cell.setCellValue("Extra");
-			cell.setCellStyle(headerStyle);
-			
-			cell = row.createCell(6);
-			cell.setCellValue("Comment");
-			cell.setCellStyle(headerStyle);
+        for (int i = 0; i < table.size(); i++) {
+            TableName name = table.get(i);
+            
+            row = sheet.createRow((short) k++);
+            cell = row.createCell(0);
+            cell.setCellValue("TableName");
+            cell.setCellStyle(headerStyle);
 
-			List<TableStruct> structList = name.getStruct();
+            cell = row.createCell(1);
+            cell.setCellStyle(headerStyle);
 
-			for (int j = 0; j < structList.size(); j++, k++) {
-				// 시트에 하나의 행을 생성한다(i 값이 0이면 첫번째 줄에 해당)
-				row = sheet.createRow((short) k);
+            cell = row.createCell(2);
+            cell.setCellValue(name.getTableName());
+            cell.setCellStyle(cellStyle1);
 
-				TableStruct struct = structList.get(j);
+            cell = row.createCell(3);
+            cell.setCellStyle(cellStyle1);
+            cell = row.createCell(4);
+            cell.setCellStyle(cellStyle1);
+            cell = row.createCell(5);
+            cell.setCellStyle(cellStyle1);
+            cell = row.createCell(6);
+            cell.setCellStyle(cellStyle1);
 
-				// 생성된 row에 컬럼을 생성한다
-				int x = 0;
-				cell = row.createCell(x++);
-				cell.setCellValue(struct.getId());
-				cell.setCellStyle(cellStyle0);
-				
-				cell = row.createCell(x++);
-				cell.setCellValue(struct.getFieldName());
-				cell.setCellStyle(cellStyle1);
-				
-				cell = row.createCell(x++);
-				cell.setCellValue(struct.getDataType());
-				cell.setCellStyle(cellStyle0);
-				
-				cell = row.createCell(x++);
-				cell.setCellValue(struct.getIsNull());
-				cell.setCellStyle(cellStyle0);
-				
-				cell = row.createCell(x++);
-				cell.setCellValue(struct.getKey());
-				cell.setCellStyle(cellStyle0);
-				
-				cell = row.createCell(x++);
-				cell.setCellValue(struct.getExtra());
-				cell.setCellStyle(cellStyle0);
-				
-				cell = row.createCell(x++);
-				cell.setCellValue(struct.getComment());
-				cell.setCellStyle(cellStyle1);
-			}
-		}
+            row = sheet.createRow((short) k++);
+            cell = row.createCell(0);
+            cell.setCellValue("Description");
+            cell.setCellStyle(headerStyle);
 
-		FileOutputStream fos = null;
-		try {
-			fos = new FileOutputStream(saveFileName);
-			// 파일을 쓴다
-			workbook.write(fos);
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, e.getLocalizedMessage(), "확인", JOptionPane.ERROR_MESSAGE);
-			e.printStackTrace();
-			return;
-		} finally {
-			if (workbook != null) {
-				try {
-					workbook.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			
-			
-			if (fos != null) {
-				try {
-					fos.close();
-				} catch (IOException e) {
-					JOptionPane.showMessageDialog(null, e.getLocalizedMessage(), "확인", JOptionPane.ERROR_MESSAGE);
-					e.printStackTrace();
-					return;
-				}
-			}
-		}
+            cell = row.createCell(1);
+            cell.setCellStyle(headerStyle);
 
-		JOptionPane.showMessageDialog(null, saveFileName + " 경로에 엑셀파일이 저장되었습니다.", "확인", JOptionPane.INFORMATION_MESSAGE);
-	}
+            cell = row.createCell(2);
+            cell.setCellValue(name.getComment());
+            cell.setCellStyle(cellStyle1);
+
+            cell = row.createCell(3);
+            cell.setCellStyle(cellStyle1);
+            cell = row.createCell(4);
+            cell.setCellStyle(cellStyle1);
+            cell = row.createCell(5);
+            cell.setCellStyle(cellStyle1);
+            cell = row.createCell(6);
+            cell.setCellStyle(cellStyle1);
+
+            row = sheet.createRow((short) k++);
+
+            cell = row.createCell(0);
+            cell.setCellValue("No");
+            cell.setCellStyle(headerStyle);
+
+            cell = row.createCell(1);
+            cell.setCellValue("FieldName");
+            cell.setCellStyle(headerStyle);
+
+            cell = row.createCell(2);
+            cell.setCellValue("DataType");
+            cell.setCellStyle(headerStyle);
+
+            cell = row.createCell(3);
+            cell.setCellValue("Null");
+            cell.setCellStyle(headerStyle);
+
+            cell = row.createCell(4);
+            cell.setCellValue("Key");
+            cell.setCellStyle(headerStyle);
+
+            cell = row.createCell(5);
+            cell.setCellValue("Extra");
+            cell.setCellStyle(headerStyle);
+
+            cell = row.createCell(6);
+            cell.setCellValue("Comment");
+            cell.setCellStyle(headerStyle);
+
+            List<TableStruct> structList = name.getStruct();
+
+            for (int j = 0; j < structList.size(); j++, k++) {
+                // 시트에 하나의 행을 생성한다(i 값이 0이면 첫번째 줄에 해당)
+                row = sheet.createRow((short) k);
+
+                TableStruct struct = structList.get(j);
+
+                // 생성된 row에 컬럼을 생성한다
+                int x = 0;
+                cell = row.createCell(x++);
+                cell.setCellValue(struct.getId());
+                cell.setCellStyle(cellStyle0);
+
+                cell = row.createCell(x++);
+                cell.setCellValue(struct.getFieldName());
+                cell.setCellStyle(cellStyle1);
+
+                cell = row.createCell(x++);
+                cell.setCellValue(struct.getDataType());
+                cell.setCellStyle(cellStyle0);
+
+                cell = row.createCell(x++);
+                cell.setCellValue(struct.getIsNull());
+                cell.setCellStyle(cellStyle0);
+
+                cell = row.createCell(x++);
+                cell.setCellValue(struct.getKey());
+                cell.setCellStyle(cellStyle0);
+
+                cell = row.createCell(x++);
+                cell.setCellValue(struct.getExtra());
+                cell.setCellStyle(cellStyle0);
+
+                cell = row.createCell(x++);
+                cell.setCellValue(struct.getComment());
+                cell.setCellStyle(cellStyle1);
+            }
+            
+            // 테이블간 빈 줄 삽입
+            row = sheet.createRow((short) k++);
+            cell = row.createCell(1);
+            cell = row.createCell(2);
+            cell = row.createCell(3);
+            cell = row.createCell(4);
+            cell = row.createCell(5);
+            cell = row.createCell(6);
+            row = sheet.createRow((short) k++);
+            cell = row.createCell(1);
+            cell = row.createCell(2);
+            cell = row.createCell(3);
+            cell = row.createCell(4);
+            cell = row.createCell(5);
+            cell = row.createCell(6);
+        }
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(saveFileName);
+            // 파일을 쓴다
+            workbook.write(fos);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getLocalizedMessage(), "확인", JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+            return;
+        } finally {
+            if (workbook != null) {
+                try {
+                    workbook.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(null, e.getLocalizedMessage(), "확인", JOptionPane.ERROR_MESSAGE);
+                    e.printStackTrace();
+                    return;
+                }
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, saveFileName + " 경로에 엑셀파일이 저장되었습니다.", "확인", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
